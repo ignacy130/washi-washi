a0dbee3d1edb9d75ec1c69db8cab564752832b78
Ignacy <ignacy130@gmail.com>
2017-03-11 20:13:48 +0100

    get posts metadata

---
+++ TumblrTagger/Credentials.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TumblrTagger
+{
+    public static class Credentials
+    {
+        public static string Key { get; set; }
+        public static string Secret { get; set; }
+    }
+}
--- TumblrTagger/Program.cs
+++ TumblrTagger/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DontPanic.TumblrSharp;
+using DontPanic.TumblrSharp.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,19 @@ namespace TumblrTagger
     {
         static void Main(string[] args)
         {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            var factory = new TumblrClientFactory();
+            var client = factory.Create<TumblrClient>(Credentials.Key, Credentials.Secret);
+            var posts = new List<BasePost>();
+            for (int i = 0; i < 100; i+=20)
+            {
+                var next = await client.GetPostsAsync("malowiele", 0, 20);
+                posts.AddRange(next.Result.ToList());
+            }
         }
     }
 }
